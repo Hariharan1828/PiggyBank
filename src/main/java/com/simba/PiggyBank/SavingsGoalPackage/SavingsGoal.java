182c58a4a86308a4298d926b7d167bcedaac82e7
Hariharan1828 <hariharanr.yr@gmail.com>
2024-03-22 01:18:06 +0530

    Constructor, Getters and Setters added

--- src/main/java/com/simba/PiggyBank/SavingsGoalPackage/SavingsGoal.java
+++ src/main/java/com/simba/PiggyBank/SavingsGoalPackage/SavingsGoal.java
@@ -28,5 +28,60 @@ public class SavingsGoal {
     @Column(nullable = false)
     private LocalDate deadline;
 
-    // Constructors, getters, and setters
+    public SavingsGoal(Long id, User user, String goalName, double targetAmount, double currentAmount, LocalDate deadline) {
+        this.id = id;
+        this.user = user;
+        this.goalName = goalName;
+        this.targetAmount = targetAmount;
+        this.currentAmount = currentAmount;
+        this.deadline = deadline;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getGoalName() {
+        return goalName;
+    }
+
+    public void setGoalName(String goalName) {
+        this.goalName = goalName;
+    }
+
+    public double getTargetAmount() {
+        return targetAmount;
+    }
+
+    public void setTargetAmount(double targetAmount) {
+        this.targetAmount = targetAmount;
+    }
+
+    public double getCurrentAmount() {
+        return currentAmount;
+    }
+
+    public void setCurrentAmount(double currentAmount) {
+        this.currentAmount = currentAmount;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
 }
